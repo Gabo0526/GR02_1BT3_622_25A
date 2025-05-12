89a9356543cf89ad623ebb92620aa1c3d5c74042
Gabo <gabriel.vasconez@epn.edu.ec>
2025-05-11 19:45:49 -0500

    Hay que usar el JDK 17 para Mockito

---
+++ src/main/java/com/ma/gr02_1bt3_622_25a/service/ServicioVerCasilleros.java
@@ -0,0 +1,10 @@
+package com.ma.gr02_1bt3_622_25a.service;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+public class ServicioVerCasilleros {
+
+    public String getViewMode(HttpServletRequest request) {
+        return "";
+    }
+}
---
+++ src/test/java/com/ma/gr02_1bt3_622_25a/service/ServicioVerCasillerosTest.java
@@ -0,0 +1,24 @@
+package com.ma.gr02_1bt3_622_25a.service;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServicioVerCasillerosTest {
+    @Test
+    public void givenNoViewMode_whenGetViewMode_thenReturnReservarViewMode() {
+        // Arrange
+        ServicioVerCasilleros servicio = new ServicioVerCasilleros();
+        HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
+        Mockito.when(request.getAttribute("modo")).thenReturn(null);
+        String expectedViewMode = "reservar";
+
+        // Act
+        String actualViewMode = servicio.getViewMode(request);
+
+        // Assert
+        assertEquals(expectedViewMode, actualViewMode);
+    }
+}
